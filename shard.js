06e96bcb7e568cc95e0392382457a95349b62206
NsNNt <94731789+NsNNt@users.noreply.github.com>
2021-11-26 22:52:22 -0600

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,1 +1,13 @@
-// shardealo aca xd
+const { ShardingManager } = require("discord.js");
+require("dotenv").config();
+
+const shards = new ShardingManager('./index.js', {
+    token: process.env.TOKEN,
+    totalShards: "auto"
+});
+
+shards.on('shardCreate', (shard) => {
+    console.log(`[${new Date().toString().split(" ", 5).join(" ")}] Iniciando shard #${shard.id}`)
+});
+
+shards.spawn(shards.totalShards, 10000)
